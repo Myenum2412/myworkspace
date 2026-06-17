03a254c7271ec98e57c0f90c56ba055cee3584c0
Myenum2412 <myenumam@gmail.com>
2026-06-17 13:51:23 +0530

    fix: lazy mongo init via Proxy to prevent build-time crash

--- frontend/lib/db/mongodb.ts
+++ frontend/lib/db/mongodb.ts
@@ -1,29 +1,38 @@
 import { MongoClient, Db } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DB || "myworkspace";
-
-if (!uri) {
-  throw new Error("MONGODB_URI is not defined");
-}
 
 const globalForMongo = globalThis as unknown as {
   client: MongoClient | undefined;
   db: Db | undefined;
 };
 
+function getUri() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGODB_URI is not defined");
+  return uri;
+}
+
 function createClient() {
-  const client = new MongoClient(uri!);
+  const client = new MongoClient(getUri());
   const db = client.db(dbName);
   return { client, db };
 }
 
-export const mongo = globalForMongo.db ?? (() => {
-  const { client, db } = createClient();
-  globalForMongo.client = client;
-  globalForMongo.db = db;
-  return db;
-})();
+function getOrCreateMongo() {
+  if (!globalForMongo.db) {
+    const { client, db } = createClient();
+    globalForMongo.client = client;
+    globalForMongo.db = db;
+  }
+  return globalForMongo.db;
+}
+
+export const mongo = new Proxy({} as Db, {
+  get(_, prop) {
+    return Reflect.get(getOrCreateMongo(), prop);
+  },
+});
 
 export async function connectToMongo() {
   if (globalForMongo.client) return { client: globalForMongo.client, db: mongo };
